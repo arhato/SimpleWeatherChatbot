e046dfd92c42e84b67779383e8726eee857e6d59
Your Name <ianpaulmunene@gmail.com>
2023-04-25 22:34:24 +0100

    Fully working user data storage

--- chatbot/src/main/java/trailblazerschatbot/chatbot/UserData.java
+++ chatbot/src/main/java/trailblazerschatbot/chatbot/UserData.java
@@ -1,19 +1,32 @@
 package trailblazerschatbot.chatbot;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
 public class UserData {
-	public ArrayList<String[]> location;
+	public int count = -1;
+    public String[][] location;
+    private ArrayList<String> days = new ArrayList<>(List.of("monday","tuesday","wednesday","thursday","friday","saturday","sunday"));
 
-    public UserData() {
-        location = new ArrayList<>();
+    public UserData(int size) {
+        this.location = new String[size][2];
     }
 
-    public ArrayList<String[]> getLocation() {
-        return location;
+    public void addCity(String input) {
+        location[count][0] = input;
     }
+    public void addDay(String day, int duration){
+        day = day.toLowerCase();
+        int index = days.indexOf(day);
+        for (int i = 0; i < duration; i++) {
+            location[count+i][0] = location[count+i][0];
+            location[count+i][1] = days.get((index+i)%7);
+        }
 
-    private void addLocation(String input) {
-    	
+    }
+    public void increment(){
+        count++;
     }
 }
